8bd2c76f77e543bd1eaea0c8559aee843a27b42b
IhorVitko <igor.vitko.74@gmail.com>
2021-08-20 17:36:41 +0300

    add token Authentication, add views -  Post, Comment, Category (CRUD)

--- tutorial/settings.py
+++ tutorial/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
+    'tutorial.quickstart.apps.QuickstartConfig',
 ]
 
 MIDDLEWARE = [
@@ -127,5 +129,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
 }
+
